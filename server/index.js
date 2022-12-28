7a812a04a35218fb438f714380028c8279156f91
acmahaja <acmahaja@gmail.com>
2022-12-28 19:38:51 +0200

    Added simple articles

--- server/index.js
+++ server/index.js
@@ -6,22 +6,24 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 var bodyParser = require("body-parser");
 
-
 app.use(cors());
+
 app.use(express.json());
 dotenv.config();
+
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
+
 app.use(bodyParser.json());
 
 mongoose
   .connect(
     process.env.NODE_ENV === "production"
       ? process.env.DATABASE
-      : "mongodb://0.0.0.0:27017/template-mern"
+      : "mongodb://0.0.0.0:27017/blog.acmahaja.com"
   )
   .then(() => {
     console.log(`🌿[Database]: Connected to database`);
@@ -38,7 +40,7 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("🖥️ Hello From MernTemplate");
+  res.send("🖥️ Hello From MernTemplate Server");
 });
 
 app.listen(port, () => {
