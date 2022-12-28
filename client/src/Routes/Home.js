90a7c9c4ab832f1eab89d9216becdc3fbcd25158
acmahaja <acmahaja@gmail.com>
2022-12-28 19:55:11 +0200

    Added webpack to use different css files

--- client/src/Routes/Home.js
+++ client/src/Routes/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+import '../css/Home.css'
+import '../css/Light/Home.css'
+import '../css/Dark/Home.css'
 
 function Home() {
     return (
-        <h2>Home</h2>
+        <div className="Light Home">
+            <h2>Home</h2>
+        </div>
     )
 }
 
